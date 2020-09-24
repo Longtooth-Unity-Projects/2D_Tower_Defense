1db409545fbaf8e053341bd095fd22efd0abb631
samhain323 <samhain323@hotmail.com>
2020-09-24 16:18:54 -0700

    Initial commit

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    [SerializeField] int splashScreenScene = 0;
+    [SerializeField] float loadScreenDelay = 3f;
+
+    private int currentScreenIndex;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentScreenIndex = SceneManager.GetActiveScene().buildIndex;
+        if (currentScreenIndex == splashScreenScene)
+        {
+            StartCoroutine(DelayLoadScene());
+        }
+    }
+
+    private IEnumerator DelayLoadScene()
+    {
+        yield return new WaitForSeconds(loadScreenDelay);
+        LoadNextScene();
+    }
+
+    private void LoadNextScene()
+    {
+        SceneManager.LoadScene(currentScreenIndex + 1);
+    }
+}
