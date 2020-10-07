0cad123d529f61b71553057ec19b60e19766b842
samhain323 <samhain323@hotmail.com>
2020-10-07 10:33:24 -0700

    Implement attacker walking script.

---
+++ Assets/Scripts/Attacker.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attacker : MonoBehaviour
+{
+    [Range(0f, 5f)] [SerializeField] float walkspeed = 1f;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //move each frame
+        transform.Translate(Vector2.left * Time.deltaTime * walkspeed);
+    }
+}
